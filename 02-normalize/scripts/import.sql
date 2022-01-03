35b0835766f3d35746fa4f319abcfcc323e8d18d
Rob Conery <rob@conery.io>
2022-01-03 11:02:23 -1000

    fixed issue with Makefile referencing wrong CSV path. Fixed 02-normalize import script to have correct schema

--- 02-normalize/scripts/import.sql
+++ 02-normalize/scripts/import.sql
@@ -1,3 +1,7 @@
+drop schema if exists import cascade;
+create schema import;
+
+
 drop table if exists events cascade;
 drop table if exists teams cascade;
 drop table if exists targets cascade;
@@ -5,8 +9,7 @@ drop table if exists spass_types cascade;
 drop table if exists requests cascade;
 drop table if exists event_types cascade;
 
-drop table if exists master_plan;
-create table master_plan(
+create table import.master_plan(
   start_time_utc text,
   duration text,
   date text,
